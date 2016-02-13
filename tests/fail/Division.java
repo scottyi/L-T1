d332300d1179a7e372b45b76afb382990fec91a8
Scott Ivinza <Scott@MacBook-Pro-de-Scott.local>
2016-02-13 14:22:05 +0100

    ajout de fichiers dans git

---
+++ tests/fail/Division.java
@@ -0,0 +1,13 @@
+// Division.java
+
+package fail;
+
+import java.lang.System;
+
+public class Division {
+	
+	public static void main(String [] args) {
+		
+		System.out.println(’a’ / 42);
+	}
+}
---
+++ tests/pass/Division.java
@@ -0,0 +1,11 @@
+// Division.java
+
+package pass;
+
+public class Division {
+	
+	public int divide(int x, int y){
+		
+		return x / y;
+	}
+}
